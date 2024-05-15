895ae2d675388b1adeaef209bc7582ee1a224e0a
Manuel <manuelloverso60@gmail.com>
2024-05-15 14:53:27 +0200

    fill create table migration and comic seeder

--- database/migrations/2024_05_15_122611_create_comics_table.php
+++ database/migrations/2024_05_15_122611_create_comics_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price', 15)->nullable();
+            $table->string('series', 100)->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type', 50)->nullable();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Comic;
 use Illuminate\Support\Facades\Route;
 
 /*
